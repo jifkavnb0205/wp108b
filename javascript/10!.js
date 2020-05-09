e6c98275b7d038a4087e787404e87163a0563da2
whfeuihgfiuerahfer <anbra0205@gmail.com>
2020-05-10 01:32:33 +0800

    add javascript

---
+++ javascript/10!.js
@@ -0,0 +1,6 @@
+sum=1
+
+for(x=1;x<=10;x++){
+    sum = sum * x;
+}
+console.log('10!',sum);
---
+++ javascript/prime.js
@@ -0,0 +1,14 @@
+function isPrime(n){
+    for(var i=2;i<n;i++){
+        if(n%i === 0)
+        return false;
+    }
+    return true;
+}
+a=8,b=20,c=0;
+for(i=a;i<=b;i++){
+    if(isPrime(i)){
+        c=c+1;
+    }
+}
+console.log('countPrime(3,7)=',c);
